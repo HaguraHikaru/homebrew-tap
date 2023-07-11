2a6d05cdbdc56f791ddf39fa40699e5192717be3
Air <hikaruair@haguraakiranoMacBook-Pro-8.local>
2023-07-11 12:19:39 +0900

    zip指定にした

--- Formula/urakil.rb
+++ Formula/urakil.rb
@@ -2,9 +2,9 @@ VERSION="0.1.5"
 class Urakil < Formula
     desc "URL shortener via bit.ly, etc."
     homepage "https://github.com/HaguraHikaru/urakil"
-    url "https://github.com/HaguraHikaru/urakil/archive/refs/tags/v0.1.5.tar.gz"
+    url "https://github.com/HaguraHikaru/urakil/releases/download/v0.1.5/urakil-0.1.5.zip"
     version VERSION
-    sha256 "45e859e88131719d62bfec2b6ee4320979608e5aadf4c3cf56afd2eda65cd581"
+    sha256 "683b61b20f3c123040fbae4869b1227829da81208fe26919e3b5766ab8580838"
     license "MIT"
     option "without-completions", "Disable bash completions"
     depends_on "bash-completion@2" => :optional
