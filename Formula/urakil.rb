a37fd9853b163038adc9d45f23e5dbdc0c31d4e3
Air <hikaruair@haguraakiranoMacBook-Pro-8.local>
2023-07-11 11:51:08 +0900

    versionの項目を追加した

--- Formula/urakil.rb
+++ Formula/urakil.rb
@@ -2,7 +2,8 @@ VERSION="0.1.5"
 class Urakil < Formula
     desc "URL shortener via bit.ly, etc."
     homepage "https://github.com/HaguraHikaru/urakil"
-    url "https://github.com/HaguraHikaru/urakil/archive/refs/tags/v0.1.5.tar.gz"
+    url "https://github.com/HaguraHikaru/urakil/archive/refs/tags/v#{VERSION}.tar.gz"
+    version VERSION
     sha256 "9acaac942b4617ac01495de24da482be902d7dc1ea05b8947452f288a248c7f9"
     license "MIT"
     option "without-completions", "Disable bash completions"
